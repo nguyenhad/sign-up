d758bbd6b211339f7652fa25bf15e102c9fa8e68
nguyenhad <your.nguyen.thi.thu.had@framgia.com>
2016-06-15 17:33:35 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit,  :update, :destroy]
   before_action :correct_user,   only: [:edit,  :update]
   before_action :admin_user,     only: :destroy
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
+
   
   def index
     @users = User.paginate(page: params[:page])
@@ -9,6 +12,7 @@ class UsersController < ApplicationController
 
     def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -45,6 +49,21 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+    
+  end
+   def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+     
+   end
 
 
   private
